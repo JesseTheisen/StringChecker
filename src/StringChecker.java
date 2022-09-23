57c56bdfc3b492a28371913be688709ad375179a
Jesse Theisen <jesse.theisen@snhu.edu>
2022-09-22 22:13:45 -0500

    created program to check and modify strings

---
+++ src/StringChecker.java
@@ -0,0 +1,13 @@
+// replace method on line 10 to manipulate strings
+
+
+public class StringChecker {
+    public static void main (String [] args) {
+        String str1 = "Banana-fana fo-f(Name)!";
+        String str2 = "Fee fi mo-maitlin";
+
+      String  newString = str2;
+      System.out.println(newString.substring(1));
+
+    }
+}
